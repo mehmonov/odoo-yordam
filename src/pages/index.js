083a3972c7efbb4edcaf4865dadba43c1db0c2a2
Husniddin <103229084+mehmonov@users.noreply.github.com>
2024-08-30 12:38:35 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Odoo qo'llanma`}
+      description="O'zbekcha odoo qo'llanma. Odoo, uzbekcha, o'zbekcha">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
